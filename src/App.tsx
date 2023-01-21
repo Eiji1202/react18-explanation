8a40daf54956017687a7768b38d4e6f44d5567a3
Eiji1202 <tsuyoku1202@gmail.com>
2023-01-21 15:37:38 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Transition } from "./components/Transition";
 import "./App.css";
+import { ReactQuery } from "./components/ReactQuery";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <div className="App">
       <Transition />
+      <hr />
+      <ErrorBoundary fallback={<p>全体エラーです！</p>}>
+        <Suspense fallback={<p>全体ローディング中だよ〜</p>}>
+          <ReactQuery />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 }
